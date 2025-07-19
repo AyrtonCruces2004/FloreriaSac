c255a05c57556214cb9cc2f7f36117389fb7f52a
Calderon Cruces Jose Ayrton <u21215967@utp.edu.pe>
2025-07-19 16:58:57 -0500

    UPDATE 19-07-2025
    Listowo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
+// Importa Swiper.js y sus estilos para el carrusel del Hero
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import './bootstrap';
